f0b4c158d68c5b940623ec7a16289d42a1425345
Cloud Custodian <joe.p.terlecki@gmail.com>
2022-03-25 23:46:46 -0400

    javascript changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,43 +1,35 @@
 function getNumbers() {
-  let startNumber = document.getElementById("startNumber").value;
-  let endNumber = document.getElementById("endNumber").value;
+    let startNumber = document.getElementById("startNumber").value;
+    let endNumber = document.getElementById("endNumber").value;
 
-  startNumber = parseInt(startNumber);
-  endNumber = parseInt(endNumber);
+    startNumber = parseInt(startNumber);
+    endNumber = parseInt(endNumber);
 
-  if (Number.isInteger(startNumber) && Number.isInteger(endNumber)) {
-    let numbers = generateNumbers(startNumber, endNumber);
+    if (Number.isInteger(startNumber) && Number.isInteger(endNumber)) {
+        generateNumberArray(startNumber, endNumber);
 
-    displayNumbers(numbers);
-
-  } else {
-    alert("You Must Enter A Valid Integer");
-  }
-}
-
-function generateNumbers(startNumber, endNumber) {
-  let numbers = [];
-
-  for (let number = startNumber; number <= endNumber; number++) {
-    numbers.push(number);
-  }
-
-  return numbers;
-}
-
-function displayNumbers(numbers) {
-  let templateRows = "";
-  for (let number = 0; number < numbers.length; number++) {
-    let displayNumber = numbers[number];
-
-    if (displayNumber % 2 === 0) {
-      templateRows += `<tr><td class="align-middle text-center"><span class="text-primary text-gradient text-xs font-weight-bold">${displayNumber}</span></td></tr>`;
     } else {
-      templateRows += `<tr><td class="align-middle text-center"><span class="text-secondary text-xs">${displayNumber}</span></td></tr>`;
+        alert("You Must Enter A Valid Integer");
     }
+}
 
-  }
-
-  document.getElementById("results").innerHTML = templateRows;
-
+function generateNumberArray(startNumber, endNumber) {
+    const url = `https://api.joeterlecki.io/0-to-100?start_number=${startNumber}&end_number=${endNumber}`
+    fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            let numbers = data["number_list"];
+            let templateRows = "";
+            for (let number = 0; number < numbers.length; number++) {
+                let displayNumber = numbers[number];
+                if (displayNumber % 2 === 0) {
+                    templateRows += `<tr><td class="align-middle text-center"><span class="text-primary text-gradient text-xs font-weight-bold">${displayNumber}</span></td></tr>`;
+                } else {
+                    templateRows += `<tr><td class="align-middle text-center"><span class="text-secondary text-xs">${displayNumber}</span></td></tr>`;
+                }
+            }
+            document.getElementById("results").innerHTML = templateRows;
+        })
 }

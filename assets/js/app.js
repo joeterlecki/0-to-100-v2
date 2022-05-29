6d3f3acf25be8005877f43acbad84e280585f7cf
Cloud Custodian <joe.p.terlecki@gmail.com>
2022-05-28 21:41:00 -0400

    refactor after traversy knoledge

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,74 @@
-function getNumbers() {
-    let startNumber = document.getElementById("startNumber").value;
-    let endNumber = document.getElementById("endNumber").value;
-
-    startNumber = parseInt(startNumber);
-    endNumber = parseInt(endNumber);
-
-    if (Number.isInteger(startNumber) && Number.isInteger(endNumber)) {
-        generateNumberArray(startNumber, endNumber);
-
-    } else {
-        alert("You Must Enter A Valid Integer");
+const NumberController = (function () {
+  return {
+    getNumberList: function (startNumber, endNumber) {
+      let numbers = [];
+      for (let number = startNumber; number <= endNumber; number++) {
+        numbers.push(number);
+      }
+      return numbers;
     }
-}
+  }
+})();
 
-function generateNumberArray(startNumber, endNumber) {
-    const url = `https://api.joeterlecki.io/0-to-100?start_number=${startNumber}&end_number=${endNumber}`
-    fetch(url)
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            let numbers = data["number_list"];
-            let templateRows = "";
-            for (let number = 0; number < numbers.length; number++) {
-                let displayNumber = numbers[number];
-                if (displayNumber % 2 === 0) {
-                    templateRows += `<tr><td class="align-middle text-center"><span class="text-primary text-gradient text-xs font-weight-bold">${displayNumber}</span></td></tr>`;
-                } else {
-                    templateRows += `<tr><td class="align-middle text-center"><span class="text-secondary text-xs">${displayNumber}</span></td></tr>`;
-                }
-            }
-            document.getElementById("results").innerHTML = templateRows;
-        })
-}
+const UIController = (function () {
+  const uiSelectors = {
+    startNumberInput: '#startNumber',
+    endNumberInput: '#endNumber',
+    submitButton: '#btn-submit',
+    tableResults: '#results',
+  }
+
+  return {
+    getSelectors: function () {
+      return uiSelectors
+    },
+    getNumberInput: function () {
+      return {
+        startNumber: document.querySelector(uiSelectors.startNumberInput).value,
+        endNumber: document.querySelector(uiSelectors.endNumberInput).value
+      }
+    },
+    setPrimaryText: function () {
+      const classList = 'text-primary text-gradient text-xs font-weight-bold';
+      return classList;
+    },
+    setSecondaryText: function () {
+      const classList = 'text-secondary text-xs';
+      return classList;
+    },
+    populateNumbers: function (numbers) {
+      let tableRows = '';
+      for (let number = 0; number < numbers.length; number++) {
+
+        if (number % 2 === 0) {
+          tableRows += `<tr><td class="align-middle text-center"><span class=${UIController.setPrimaryText()}>${number}</span></td></tr>`;
+        } else {
+          tableRows += `<tr><td class="align-middle text-center"><span class=${UIController.setSecondaryText()}>${number}</span></td></tr>`;
+        }      }
+      document.querySelector(uiSelectors.tableResults).innerHTML = tableRows;
+    }
+  }
+})();
+
+const AppController = (function (UIController, NumberController) {
+  const loadEventListeners = function () {
+    const uiSelectors = UIController.getSelectors()
+    document.querySelector(uiSelectors.submitButton).addEventListener('click', submitNumbers )
+  }
+
+  const submitNumbers = function (event) {
+    const input = UIController.getNumberInput();
+    const numberArray = NumberController.getNumberList(input.startNumber, input.endNumber);
+    UIController.populateNumbers(numberArray);
+
+    event.preventDefault();
+  }
+
+  return {
+    init: function () {
+      loadEventListeners();
+    }
+  }
+})(UIController, NumberController);
+
+AppController.init();
